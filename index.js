bc79721b828f11981bd7b730fa062824330263b2
Gohar Ali <115146848+go-har-ali@users.noreply.github.com>
2024-08-02 11:06:52 +0500

    Method Chaining, Loops, Logical Operators

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Counting with buttons
-
+//console.log('Hello World!');
 const decreaseBtn = document.getElementById('dec-btn');
 const resetBtn = document.getElementById('reset-btn');
 const increaseBtn = document.getElementById('inc-btn');
@@ -157,3 +157,161 @@ console.log(username.length);
 username = username.trim();
 console.log(username);
 
+// String Slicing in JS
+const fullName = 'John Doe';
+
+let firstName = fullName.slice(0, 4);
+
+let lastName = fullName.slice(5);
+
+console.log(lastName);
+
+let firstChar = fullName.slice(0, 1);
+
+let lastChar = fullName.slice(-1);
+
+console.log(firstName); 
+
+const email = "gohar@gmail.com";
+
+let username2 = email.slice(0, email.indexOf('@'));
+let extension = email.slice(email.indexOf('@') + 1);
+console.log(username2);
+console.log(extension);
+
+// Method Chaining
+
+let username3 = window.prompt('Enter your username: ');
+
+username3 = username3.trim();
+let letter = username3.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChars = username3.slice(1);
+extraChars = extraChars.toLowerCase();
+username3 = letter + extraChars;
+
+console.log(username3);
+
+// Method Chaining
+
+let username4 = window.prompt('Enter your username: ');
+username4 = username4.trim().charAt(0).toUpperCase() + username4.slice(1).toLowerCase();
+console.log(username4);
+
+// Logical Operators
+
+const temp = 25;
+
+if (temp > 0) {
+    console.log('The temperature is Good');
+}
+else if (temp < 30) {
+    console.log('The temperature is Good');
+}
+else {
+    console.log('The temperature is Bad');
+}
+
+// Using Logical Operators
+if (temp > 0 && temp < 30) {
+    console.log('The temperature is Good');
+}
+
+if (temp < 0 || temp > 30) {
+    console.log('The temperature is Bad');
+}
+else {
+    console.log('The temperature is Good');
+}
+
+const isSunny = true;
+
+if (!isSunny){
+    console.log('It is cloudy');
+}
+else {
+    console.log('It is sunny');
+}
+
+// = assignment operator
+// == equality operator (checks only value)
+// === strict equality operator (checks value and type)
+// != inequality operator
+// !== strict inequality operator
+
+const PI = 3.14;
+
+if (PI === "3.14") {
+    console.log('The value of PI is 3.14');
+}
+else {
+    console.log('The value of PI is not 3.14');
+}
+
+if (PI !== "3.14") {
+    console.log('The value of PI is not 3.14');
+}
+else {
+    console.log('The value of PI is 3.14');
+}
+
+if (PI === 3.14) {
+    console.log('The value of PI is 3.14');
+}else {
+    console.log('The value of PI is not 3.14');
+}
+
+// While Loop in JS
+let username5 = "";
+
+while (username5 === "" || username5 === null) {
+    username5 = window.prompt('Enter your username: ');
+}
+
+console.log(`Welcome ${username5}`);
+
+let loggedIn = false;
+let username6;
+let password;
+
+while (!loggedIn) {
+    username6 = window.prompt('Enter your username: ');
+    password = window.prompt('Enter your password: ');
+
+    if (username6 === 'admin' && password === '12345') {
+        loggedIn = true;
+        console.log('You are logged in');
+    }
+    else {
+        console.log('Invalid username or password');
+    }
+}
+
+// For Loop in JS
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+for (let i = 5; i > 0; i--) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 13) {
+        continue;
+    }
+    else {
+        console.log(i);
+    }
+}
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 13) {
+        break;
+    }
+    else {
+        console.log(i);
+    }
+}
