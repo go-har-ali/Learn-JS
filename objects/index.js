75e401386176f7128a50dff0ebd452abf16dd215
Gohar Ali <115146848+go-har-ali@users.noreply.github.com>
2024-08-08 10:12:18 +0500

    objects in JS

--- objects/index.js
+++ objects/index.js
@@ -1,1 +1,46 @@
+// objects in JS
 
+const person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    age: 30,
+    isEmployed: true,
+    sayHello: function() {
+        console.log('Hi! I am ' + this.firstname + ' ' + this.lastname)
+    },
+    sayBye: function() {
+        console.log('Goodbye!')
+    },
+    eat: () => { 
+        console.log('I am eating pizza')
+    }
+};
+
+const person2 = {
+    firstname: 'Jane',
+    lastname: 'Doe',
+    age: 25,
+    isEmployed: false,
+    sayHello: function() {
+        console.log('Hi! I am ' + this.firstname + ' ' + this.lastname)
+    },
+    sayBye: function() {
+        console.log('Goodbye!')
+    },
+    eat: () => { 
+        console.log('I am eating sushi')
+    }
+};
+
+person.sayHello();
+person.sayBye();
+person.eat();
+
+person2.sayHello();
+person2.sayBye();
+person2.eat();
+
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+console.log(person.isEmployed);
