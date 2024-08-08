2861a4b0a42c5d0670b80b2f0962caa5d288073a
Gohar Ali <115146848+go-har-ali@users.noreply.github.com>
2024-08-08 10:42:12 +0500

    classes in JS

--- classes/index.js
+++ classes/index.js
@@ -1,1 +1,37 @@
+// classes in JS
 
+class Products{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+
+    displayProduct(){
+        console.log(`Product name: ${this.name} and price: ${this.price}`);
+    }
+
+    calculateTax(salesTax){
+        return this.price * salesTax;
+    }
+}
+
+const salesTax = 0.05;
+
+const product1 = new Products('Laptop', 1000);
+const product2 = new Products('Mobile', 500);
+const product3 = new Products('Tablet', 300);
+
+
+product1.displayProduct();
+product2.displayProduct();
+product3.displayProduct();
+
+const totalForProduct1 = product1.calculateTax(salesTax);
+
+console.log(`Total for product1: ${totalForProduct1}`);
+
+const  totalForProduct2 = product2.calculateTax(salesTax);
+console.log(`Total for product2: ${totalForProduct2}`);
+
+const totalForProduct3 = product3.calculateTax(salesTax);
+console.log(`Total for product3: ${totalForProduct3}`);
