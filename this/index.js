f465d7a107735484d695cd7d4742d579a056c39a
Gohar Ali <115146848+go-har-ali@users.noreply.github.com>
2024-08-08 10:21:51 +0500

    this in JS

--- this/index.js
+++ this/index.js
@@ -1,1 +1,25 @@
+// this in JS
+
+// this in JS is a reference to the object that is executing the current function
+
+const person = {
+    name: 'John',
+    age: 30,
+    greet() {
+        console.log('Hello, my name is ' + this.name);
+    }
+};
+
+person.greet(); // Hello, my name is John.
+
+const person2 = {
+    name: 'Ali',
+    age: 30,
+    sayHello: () => {
+        console.log(`Hello, my name is ${person2.name}`);
+    }
+};
+
+person2.sayHello(); // Hello, my name is undefined
+
 
